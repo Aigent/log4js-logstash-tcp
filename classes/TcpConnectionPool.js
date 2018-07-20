915af651818778a4117e6ba354559d8e6a718f8a
Sebastian Lagemann <sebastian.lagemann@iqu.com>
2018-07-21 00:57:55 +0200

    ensure that logstash connection was established before sending data

--- classes/TcpConnectionPool.js
+++ classes/TcpConnectionPool.js
@@ -3,12 +3,15 @@
 const net = require("net");
 const EventEmitter = require('events');
 
-const MAX_TCP_CONNECITONS = 300;
+const DEFAULT_MAX_TCP_CONNECTIONS = 300;
 
 class TcpConnectionWrapper extends EventEmitter {
 
-    constructor(host, port) {
+    constructor(host, port, config) {
         super();
+
+        this.connectionNotReadyRetryInterval = parseInt(config.connectionNotReadyRetryInterval || 500);
+        this.connectionTimeout = parseInt(config.connectionTimeout || 5000);
 
         const self = this;
         this.connection = net.connect({
@@ -18,11 +21,11 @@ class TcpConnectionWrapper extends EventEmitter {
             self.connected = true;
         });
 
-        this.connection.setTimeout(5000);
+        this.connection.setTimeout(this.connectionTimeout);
         this.connection.on("timeout", () => {self.destroy()});
         this.connection.on("close", () => {self.destroy()});
         this.connection.on("error", (err) => {
-            console.error(`Error with the connection to ${host}:${port}. ${err}`);
+            console.error(`logstash Error with the connection to ${host}:${port}. ${err}`);
             // From docs: "The close event will be called just after this one
         });
     }
@@ -36,7 +39,16 @@ class TcpConnectionWrapper extends EventEmitter {
             }
         } )(logObject);
 
-        this.connection.write(message)
+        this.realSend(message);
+    }
+    realSend(message) {
+        const self = this;
+        if(this.connected === true) {
+            this.connection.write(message)
+        } else {
+            setTimeout(() => {self.realSend(message);}, this.connectionNotReadyRetryInterval);
+        }
+
     }
 
     destroy() {
@@ -50,8 +62,14 @@ class TcpConnectionWrapper extends EventEmitter {
 
 class TcpConnectionPool {
 
-    constructor() {
+    constructor(config) {
         this.tcpConnections = {}
+        if(config) this.config = config;
+        else this.config = {};
+
+        if(!this.config.maxTcpConnections) {
+            this.config.maxTcpConnections = DEFAULT_MAX_TCP_CONNECTIONS;
+        }
 
     }
 
@@ -60,13 +78,13 @@ class TcpConnectionPool {
     }
 
     _getTcpConnection(host, port) {
-        const index = TcpConnectionPool._createIndexForTcpConnection();
+        const index = this._createIndexForTcpConnection();
         return this.tcpConnections[index] || this._createTcpConnection(index, host, port);
     }
 
     _createTcpConnection(index, host, port) {
         const self = this;
-        const tcpConnectionWrapper = new TcpConnectionWrapper(host, port);
+        const tcpConnectionWrapper = new TcpConnectionWrapper(host, port, this.config);
         this.tcpConnections[index] = tcpConnectionWrapper;
 
         tcpConnectionWrapper.on("destroy", () => {
@@ -76,8 +94,8 @@ class TcpConnectionPool {
         return tcpConnectionWrapper;
     }
 
-    static _createIndexForTcpConnection() {
-        return Math.floor( Math.random() * MAX_TCP_CONNECITONS) % MAX_TCP_CONNECITONS;
+    _createIndexForTcpConnection() {
+        return Math.floor( Math.random() * this.config.maxTcpConnections) % this.config.maxTcpConnections;
     }
 
 }
